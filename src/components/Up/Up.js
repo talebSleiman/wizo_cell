97824428609d08a71011827c3100775989a7a30d
talebSleiman <talebsleiman4@gmail.com>
2023-06-08 16:45:40 +0300

    add go to top

--- src/components/Up/Up.js
+++ src/components/Up/Up.js
@@ -5,27 +5,36 @@ export default function Up() {
 
     const [showButton, setShowButton] = useState(false);
 
-    useEffect(() => {
-        function handleScroll() {
-            if (window.scrollY > 500) {
-                setShowButton(true);
-            } else {
-                setShowButton(false);
-            }
+    const handleScroll = () => {
+        if (window.scrollY > 500) {
+            setShowButton(true);
+        } else {
+            setShowButton(false);
         }
+    }
+
+    useEffect(() => {
         window.addEventListener('scroll', handleScroll);
         return () => {
             window.removeEventListener('scroll', handleScroll);
         };
     }, []);
 
+    const handleClick = () => {
+        window.scrollTo({ top: 0, behavior: 'smooth' });
+    };
+
     if (showButton) {
         return (
-            <button className="btn btn-primary up show" onClick={() => window.scrollTo({ top: 0, behavior: 'smooth' })}> Up</button>
+            <button className="btn btn-lg btn-outline-primary btn-lg-square up show" onClick={handleClick}>
+                <i className="fa fa-angle-double-up"></i>
+            </button>
         );
     } else {
         return (
-            <button className="btn btn-primary up" onClick={() => window.scrollTo({ top: 0, behavior: 'smooth' })}> Up</button>
+            <button className="btn btn-lg btn-outline-primary btn-lg-square up" onClick={handleClick}>
+                <i className="fa fa-angle-double-up"></i>
+            </button>
         );
     }
 }
